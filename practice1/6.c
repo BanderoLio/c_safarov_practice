5812df6018e71092d64af3b194d9583bf066b5e9
BanderoLio <ganshin1vladislav@gmail.com>
2025-02-13 14:20:12 +0300

    Totally correct 6

--- practice1/6.c
+++ practice1/6.c
@@ -37,8 +37,8 @@ int main() {
     scanf("%d", &n);
     char flag = 0;
     for (int i = 0; i <= n / 4; ++i) {
-        for (int j = 0; j < n / 3; ++j) {
-            for (int k = 0; k < n / 2; ++k) {
+        for (int j = 0; j <= n / 3; ++j) {
+            for (int k = 0; k <= n / 2; ++k) {
                 if (i * 4 + j * 3 + k * 2 == n) {
                     printf("Possible combination with %d 4-space ships, %d 3-space ships, %d 2-space ships\n", i, j, k);
                     flag = 1;
