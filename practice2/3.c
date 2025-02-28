c49d8ee8f91e8007f16f0c11ae33f472ed2541f4
Ruslan Safaroff <62283143+NimblePython@users.noreply.github.com>
2025-02-28 14:22:35 +0300

    Update 3.c

--- practice2/3.c
+++ practice2/3.c
@@ -17,7 +17,7 @@ int main() {
     scanf("%d", &n);
     Event* events = (Event*) malloc(sizeof(Event) * n);
     for (int i = 0; i < n; ++i) {
-        scanf("%s%d%d%d%s", events[i].name, &events[i].date.day, &events[i].date.month,
+        scanf("%s%d%d%d%255s", events[i].name, &events[i].date.day, &events[i].date.month, // теперь мы знаем, что лучше указывать число символов между % и s
          &events[i].date.year, events[i].description);
     }
     for (int i = 0; i < n; ++i) {
