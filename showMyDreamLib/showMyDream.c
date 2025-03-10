457098f80f7e83fe7c22b5a48d2ef3d9235805d3
BanderoLio <ganshin1vladislav@gmail.com>
2025-03-10 11:02:31 +0300

    Library show my dream

---
+++ showMyDreamLib/showMyDream.c
@@ -0,0 +1,10 @@
+#include "showMyDream.h"
+
+
+void showMyDream() { 
+    puts("Kill Microsoft, period.");
+}
+
+void sayMyName() {
+    puts("Heisenburger");
+}
---
+++ showMyDreamLib/usage.c
@@ -0,0 +1,5 @@
+#include "showMyDream.h"
+
+int main() {
+    showMyDream();
+}
